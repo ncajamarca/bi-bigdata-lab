e8911be76d535b01645947eb832f124e00117aab
N. Cajamarca <ncajamarca@ucvvirtual.edu.pe>
2026-09-10 22:41:14 -0500

    chore: update databricks connection properties

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -3,3 +3,10 @@
 --changeset ncajamarca:1
 CREATE SCHEMA IF NOT EXISTS bi_lab_cajamarca;
 
+--changeset ncajamarca:2
+CREATE TABLE IF NOT EXISTS bi_lab_cajamarca.alumnos (
+    id INT,
+    nombre STRING,
+    carrera STRING
+);
+
