e3c91eeb96533823febb11c981c45dc9c6650f53
N. Cajamarca <ncajamarca@ucvvirtual.edu.pe>
2026-09-19 13:09:48 -0500

    feat: configuracion CI/CD y changelogs para Cajamarca

--- changelog/db.changelog-master.sql
+++ changelog/db.changelog-master.sql
@@ -1,1 +1,10 @@
-﻿--liquibase formatted sql`n`n--changeset ncajamarca:001`nCREATE SCHEMA IF NOT EXISTS main.bi_lab_cajamarca COMMENT 'Laboratorio 02 - BI y Big Data - UCV';`n`n--rollback DROP SCHEMA IF EXISTS main.bi_lab_cajamarca;
+﻿--liquibase formatted sql
+--changeset estudiante:001
+CREATE SCHEMA IF NOT EXISTS workspace.bi_lab_7003278197
+COMMENT 'Laboratorio 02 - BI y Big Data - UCV';
+--rollback DROP SCHEMA IF EXISTS workspace.bi_lab_7003278197;
+--changeset estudiante:002
+CREATE SCHEMA IF NOT EXISTS workspace.bi_staging_7003278197
+COMMENT 'Staging schema - BI and Big Data - Lab 03';
+
+--rollback DROP SCHEMA IF EXISTS workspace.bi_staging_7003278197;
